d30d1582662c9db62eda187acdcf8b8cdffa233d
Ahn <abu89@naver.com>
2022-03-17 22:20:08 +0900

    [JPA BASIC PRACTICE] 데이터 중심 설계
     + 객체 설계를 테이블 설계에 맞춘 방식
     + 테이블의 외래키를 객체에 그대로 가져온다
     + 객체 그래프 탐색이 불가능하다

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 데이터 중심 설계
+            // 객체지향스럽지 않고 order가 주문한 member를 찾으려면
+            // memberId를 찾고 또 member를 찾아야함
+            // sudo code
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+            Member member = em.find(Member.class, memberId);
+            String name = member.getName();
+            //
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+
+}
